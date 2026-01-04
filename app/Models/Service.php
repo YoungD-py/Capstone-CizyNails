ea4561c14535772aed23173ddf4b84d2859bf365
badfellas <deruanggoro26@gmail.com>
2026-01-04 20:55:33 +0700

    Update: service models

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -14,6 +14,7 @@ class Service extends Model
         'type',
         'subtype',
         'description',
+        'image_path',
         'duration_minutes',
         'staff_count',
         'price',
